967935f2b33088f145246517320ece706f2d3aae
mileyko.ua <mileyko.ua@gmail.com>
2020-11-06 21:04:53 +0300

    Added ProgramCodeQuestion and view

---
+++ src/Teams/Controllers/ProgramCodeQuestionController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Teams.Data.Repositories;
+using Teams.Domain;
+using Teams.Models;
+using static System.IO.File;
+
+namespace Teams.Controllers
+{
+    public class ProgramCodeQuestionController : Controller
+    {
+        private IProgramCodeQuestionRepository questionRepository;
+        public ProgramCodeQuestionController(IProgramCodeQuestionRepository questionRepository)
+        {
+            this.questionRepository = questionRepository;
+        }
+        [HttpGet]
+        public IActionResult Index(Guid id)
+        {
+            var question = questionRepository.PickById(id);
+            var model = new ProgramCodeQuestionViewModel()
+            {
+                Id = question.Id,
+                Text = question.Text
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Index(ProgramCodeQuestionViewModel model)
+        {
+            // Some actions to implement
+            return Content($"The file for {model.Text} uploaded");
+        }
+    }
+}
---
+++ src/Teams/Domain/ProgramCodeQuestion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teams.Domain
+{
+    public class ProgramCodeQuestion : Question
+    {
+        private Dictionary<string, string> inputsOutputsPairs;
+        public IReadOnlyDictionary<string, string> InputsOutputsPairs => inputsOutputsPairs;
+        public ProgramCodeQuestion(string text) : base(text)
+        {
+
+        }
+        public ProgramCodeQuestion(string text, Dictionary<string, string> inputsOutputsPairs) : base(text)
+        {
+            this.inputsOutputsPairs = inputsOutputsPairs;
+        }
+    }
+}
