ee4926adff3d98f6e37a97ce455717c75b2b9c00
ShortQuality <artiksv2000@gmail.com>
2021-02-28 21:12:17 +0300

    Add UnitOfWork empty files

---
+++ src/Lab.Quiz.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab.Quiz.DAL.Interfaces
+{
+    class IUnitOfWork
+    {
+    }
+}
---
+++ src/Lab.Quiz.DAL/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Quiz.DAL.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        public TEntity GetById(Guid id);
+        public Task<TEntity> GetByIdAsync(Guid id);
+        public IEnumerable<TEntity> GetAll();
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public void Add(TEntity entity);
+        public void AddAsync(TEntity entity);
+        public void Update(TEntity entity);
+    }
+}
