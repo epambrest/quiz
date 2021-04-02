2b5cc85fd8107024d244cd540d779c30bf788ddb
isalzh <ihar_salzhanitsyn@epam.com>
2021-04-02 21:45:50 +0300

    remove blank line

--- src/Lab.Quiz.BL/Services/TestCardService/ITestCardService.cs
+++ src/Lab.Quiz.BL/Services/TestCardService/ITestCardService.cs
@@ -4,7 +4,6 @@ using Lab.Quiz.BL.Services.TestCardService.Models;
 
 namespace Lab.Quiz.BL.Services.TestCardService
 {
-
     public interface ITestCardService
     {
         Task<ICollection<TestCardModel>> GetTests();
