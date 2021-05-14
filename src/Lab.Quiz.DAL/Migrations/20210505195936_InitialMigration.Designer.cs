2ae781fac80b64a6aaecb0dcbff83b4329d3dbcc
Razrmax <61849271+Razrmax@users.noreply.github.com>
2021-05-14 16:54:34 +0200

    Created a new clean migration, removed all previous migrations too (#80)
    Co-authored-by: Максим Сулковский <maximsoolkovskiy@gmail.com>

--- src/Lab.Quiz.DAL/Migrations/20210505195936_InitialMigration.Designer.cs
+++ src/Lab.Quiz.DAL/Migrations/20210505195936_InitialMigration.Designer.cs
@@ -4,14 +4,16 @@ using Lab.Quiz.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lab.Quiz.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210505195936_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
